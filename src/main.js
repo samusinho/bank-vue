2464cbcb87f6c50584f70ec9d02e0d4059645774
Samuel Pérez <samusinho79@gmail.com>
2021-12-06 06:49:18 -0500

    config: configuración cliente apollo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,38 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { setContext } from '@apollo/client/link/context';
+import {createApolloProvider} from '@vue/apollo-option';
 
-createApp(App).mount('#app')
+const httpLink = createHttpLink({
+    uri: 'localhost:4000'
+});
+
+const authLink = setContext((_, {headers}) => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+        return {
+            headers: {
+                ...headers,
+                'Authorization': `Bearer ${token}`
+            }
+        };
+    } else {
+        return headers;
+    }
+});
+
+const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+});
+
+const apolloProvider = new createApolloProvider({
+    defaultClient: apolloClient
+});
+
+const app = createApp(App);
+
+app.use(apolloProvider);
+
+app.mount('#app')
